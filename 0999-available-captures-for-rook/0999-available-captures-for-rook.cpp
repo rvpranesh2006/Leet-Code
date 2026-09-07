33310f2fddd7fafaff552949b7a1dc1fc8dfc2cf
PRANESH R V <praneshrangasamy2006@gmail.com>
2026-09-07 16:38:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (23.25%) - LeetHub

---
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int numRookCaptures(vector<vector<char>>& board) {
+        int row ,col;
+        for(int i = 0; i < 8;i++)
+        {
+            for(int j = 0; j < 8;j++)
+            {
+                if(board[i][j] == 'R')
+                {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = row-1;i >= 0;i--)
+        {
+            if(board[i][col] == 'B')
+            {
+                break;
+            }
+            if(board[i][col] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for(int i = row +1;i < 8; i++)
+        {
+            if(board[i][col] == 'B')
+            {
+                break;
+            }
+            if(board[i][col] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for(int j = col - 1 ; j >= 0;j--)
+        {
+            if(board[row][j] == 'B')
+            break;
+            if(board[row][j] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for(int  j = col +1; j < 8; j++)
+        {
+            if(board[row][j] == 'B')
+            break;
+            if(board[row][j] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+};
