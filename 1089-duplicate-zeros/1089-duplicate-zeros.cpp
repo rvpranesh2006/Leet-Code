51b0ac58a21db1481151e06bf6a8378e0bc937d1
PRANESH R V <praneshrangasamy2006@gmail.com>
2026-09-16 22:18:00 +0530

    Time: 12 ms (14.96%), Space: 14.1 MB (50.05%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i = 0; i < arr.size();i++)
+        {
+            if(arr[i] == 0)
+            {
+                for(int j = arr.size()-1;j > i;j--)
+                {
+                    arr[j] = arr[j - 1];
+                }
+                if(i + 1 < arr.size())
+                {
+                    arr[i + 1] = 0;
+                }
+                i ++;
+            }
+        }
+        
+    }
+};
